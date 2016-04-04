34922b297fb9b3596e17a9361c8592981d991d9f
Chase Isley <isleychase@gmail.com>
2016-04-03 19:20:01 -0700

    ngIf and ngFor directives used

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,15 +11,17 @@ export class Hero {
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-        <li *ngFor="#hero of heroes">
+        <li *ngFor="#hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
             <span class="badge">{{hero.id}}</span> {{hero.name}}
         </li>
     </ul>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
+    <div *ngIf="selectedHero">
+        <h2>{{selectedHero.name}} details!</h2>
+        <div><label>id: </label>{{selectedHero.id}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="selectedHero.name" placeholder="name">
+        </div>
     </div>
     `,
     styles:[`
@@ -74,10 +76,12 @@ export class Hero {
 })
 export class AppComponent { 
     title = "Tour of Heroes";
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+    selectedHero: Hero;
+    // hero: Hero = {
+    //     id: 1,
+    //     name: 'Windstorm'
+    // };
+    onSelect(hero: Hero) { this.selectedHero = hero };
     public heroes = HEROES;
 }
 
