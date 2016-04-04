66a6010ee4d15f45de074f06626858a32c70b244
Chase Isley <isleychase@gmail.com>
2016-04-04 08:46:07 -0700

    Added new stylesheet for the app.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -34,6 +34,7 @@ import { HeroDetailComponent } from './hero-detail.component';
         </nav>
         <router-outlet></router-outlet>
     `,
+    styleUrls: ['app/app.component.css'],
     directives: [
         ROUTER_DIRECTIVES,
         // HeroesComponent - removed because we don't want this component showing the heroes.
