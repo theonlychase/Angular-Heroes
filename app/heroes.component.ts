3ebf17a3fe2c0317fc4acfca7a2fd7ea815c6efd
Chase Isley <isleychase@gmail.com>
2016-04-04 08:13:59 -0700

    imported router to heroes.components

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from 'angular2/core';
+import { Router } from 'angular2/router';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroService} from './hero.service';
