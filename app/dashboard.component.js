885493c1c0d52544b2a03a90e229afca72b2f217
Chase Isley <isleychase@gmail.com>
2016-04-03 22:44:17 -0700

    added OnInit to DashboardComponent

--- app/dashboard.component.js
+++ app/dashboard.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', './hero.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,23 +10,34 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, hero_service_1;
     var DashboardComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (hero_service_1_1) {
+                hero_service_1 = hero_service_1_1;
             }],
         execute: function() {
             DashboardComponent = (function () {
-                function DashboardComponent() {
+                function DashboardComponent(_heroService) {
+                    this._heroService = _heroService;
+                    this.heroes = [];
                 }
+                DashboardComponent.prototype.ngOnInit = function () {
+                    var _this = this;
+                    this._heroService.getHeroes()
+                        .then(function (heroes) { return _this.heroes = heroes.slice(1, 5); });
+                };
+                DashboardComponent.prototype.gotoDetail = function () { };
                 DashboardComponent = __decorate([
                     core_1.Component({
                         selector: 'my-dashboard',
                         templateUrl: 'app/dashboard.component.html'
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [hero_service_1.HeroService])
                 ], DashboardComponent);
                 return DashboardComponent;
             }());
--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,7 +1,21 @@
-import { Component } from 'angular2/core';
+import { Component, OnInit } from 'angular2/core'; //OnInit initializes the heroes
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-dashboard',
     templateUrl: 'app/dashboard.component.html'
 })
-export class DashboardComponent { }
+export class DashboardComponent implements OnInit {
+    heroes: Hero[] = [];
+    
+    constructor(private _heroService: HeroService) { }
+    
+    ngOnInit() {
+        this._heroService.getHeroes()
+            .then(heroes => this.heroes = heroes.slice(1,5));
+    }
+    
+    gotoDetail() {}
+ }
