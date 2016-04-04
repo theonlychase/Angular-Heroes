e5f94357c3625f5899b9503a38f689d86a55fe6a
Chase Isley <isleychase@gmail.com>
2016-04-04 07:42:22 -0700

    Added hero-detail.component.html and added the template url to hero-detail.component.ts

--- app/hero-detail.component.js
+++ app/hero-detail.component.js
@@ -50,7 +50,7 @@ System.register(['angular2/core', "./hero", 'angular2/router', './hero.service']
                 HeroDetailComponent = __decorate([
                     core_1.Component({
                         selector: 'my-hero-detail',
-                        template: "\n    <div *ngIf=\"hero\">\n        <h2>{{hero.name}} details!</h2>\n        <div><label>id: </label>{{hero.id}}</div>\n        <div>\n            <label>name: </label>\n            <input [(ngModel)]=\"hero.name\" placeholder=\"name\">\n        </div>\n        <button (click)=\"goBack()\">back</button>\n    </div>\n    "
+                        templateUrl: 'app/hero-detail.component.html'
                     }), 
                     __metadata('design:paramtypes', [hero_service_1.HeroService, router_1.RouteParams])
                 ], HeroDetailComponent);
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -6,17 +6,7 @@ import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-hero-detail',
-    template:`
-    <div *ngIf="hero">
-        <h2>{{hero.name}} details!</h2>
-        <div><label>id: </label>{{hero.id}}</div>
-        <div>
-            <label>name: </label>
-            <input [(ngModel)]="hero.name" placeholder="name">
-        </div>
-        <button (click)="goBack()">back</button>
-    </div>
-    `
+    templateUrl: 'app/hero-detail.component.html'
 })
 export class HeroDetailComponent implements OnInit { //export the class to make it available to other components
     @Input()
