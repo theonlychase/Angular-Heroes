24e9f9d967c0f4e48a137028113fa1c8a9424760
Chase Isley <isleychase@gmail.com>
2016-04-03 20:30:05 -0700

    added getHeroes method to HeroService

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -4,5 +4,7 @@ import {Injectable} from 'angular2/core';
 @Injectable() //Typescript sees the @Injectable decorator and emits metadata about our service, metadata that Angular may need to
               //inject other dependencies into this service. 
 export class HeroService {
-    
+    getHeroes() {
+        return HEROES;
+    }
 }
