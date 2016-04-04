ecac76c74d8819d061462a4c446fbdeed9fe523f
Chase Isley <isleychase@gmail.com>
2016-04-04 08:12:57 -0700

    Added heroes.component html and css files and removed those elements from the actual Componenet

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -5,77 +5,16 @@ import {HeroService} from './hero.service';
 
 @Component({
     selector: 'my-heroes',
-    template: `
-    <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
-    <ul class="heroes">
-        <li *ngFor="#hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
-            <span class="badge">{{hero.id}}</span> {{hero.name}}
-        </li>
-    </ul>
-    <div *ngIf="selectedHero">
-        <h2>
-            {{selectedHero.name | uppercase}} is my hero
-        </h2>
-        <button (click)="gotoDetail()">View Details</button>
-    </div>
-    `, //The two components won't coordinate until we bind the selectedHero property of the AppComponent to the HeroDetailComponent element's 
+    template: 'app/heroes.component.html',
+       //The two components won't coordinate until we bind the selectedHero property of the AppComponent to the HeroDetailComponent element's 
        //hero property
        //The [hero] property is the 'target' of a property binding. Angular insists that we declare a 'target' property to be an 'input'
        //property. Because of this binding, the HeroDetailComponent should receive the [hero] from the App Component and display that hero's
        //detail beneath the list. The detail should update every time the user picks a new hero.
-    styles:[`
-  .selected {
-    background-color: #CFD8DC !important;
-    color: white;
-  }
-  .heroes {
-    margin: 0 0 2em 0;
-    list-style-type: none;
-    padding: 0;
-    width: 15em;
-  }
-  .heroes li {
-    cursor: pointer;
-    position: relative;
-    left: 0;
-    background-color: #EEE;
-    margin: .5em;
-    padding: .3em 0;
-    height: 1.6em;
-    border-radius: 4px;
-  }
-  .heroes li.selected:hover {
-    background-color: #BBD8DC !important;
-    color: white;
-  }
-  .heroes li:hover {
-    color: #607D8B;
-    background-color: #DDD;
-    left: .1em;
-  }
-  .heroes .text {
-    position: relative;
-    top: -3px;
-  }
-  .heroes .badge {
-    display: inline-block;
-    font-size: small;
-    color: white;
-    padding: 0.8em 0.7em 0 0.7em;
-    background-color: #607D8B;
-    line-height: 1em;
-    position: relative;
-    left: -1px;
-    top: -4px;
-    height: 1.8em;
-    margin-right: .8em;
-    border-radius: 4px 0 0 4px;
-  }
-`],
-directives: [HeroDetailComponent] //Browser & Angular ignores the html tag unless we specify it here. 
-// providers: [] //Angular Dependency Injector - Here, we teach the injector how to make a HeroService by registering a HeroService provider. 
-//the providers array tells angular to create a fresh instance of the HeroService when it creates a new AppComponent. 
+    styleUrls: ['app.heroes.component.css'],
+    directives: [HeroDetailComponent] //Browser & Angular ignores the html tag unless we specify it here. 
+                                      // providers: [] //Angular Dependency Injector - Here, we teach the injector how to make a HeroService by registering a HeroService provider. 
+                                      //the providers array tells angular to create a fresh instance of the HeroService when it creates a new AppComponent. 
 })
 export class HeroesComponent implements OnInit { //Component Lifecycle - At creation, After each change, eventual destruction. Angular calls it at the right time. 
     title = "Tour of Heroes";
