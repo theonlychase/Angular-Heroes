b187463b2b34bdc612267672b7c2059a25f84e48
Chase Isley <isleychase@gmail.com>
2016-04-03 18:17:00 -0700

    Added array of HEROES

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,39 @@
 import {Component} from 'angular2/core';
 
+export class Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `
 })
-export class AppComponent { }
+export class AppComponent { 
+    title = "Tour of Heroes";
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
+
+var HEROES: Hero[] = [
+    {"id": 11, "name": "Mr. Nice" },
+    {"id": 12, "name": "Narco" },
+    {"id": 13, "name": "Bombasto" },
+    {"id": 14, "name": "Celeritas" },
+    {"id": 15, "name": "Magneta" },
+    {"id": 16, "name": "Rubberman" },
+    {"id": 17, "name": "Dynama" },
+    {"id": 18, "name": "Dr. IQ" },
+    {"id": 19, "name": "Magma" },
+    {"id": 20, "name": "Tornado" },
+]
